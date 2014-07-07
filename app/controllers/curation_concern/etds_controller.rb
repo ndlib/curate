e2ff98ccf24eb7845c29db629e27337592bb4cf5
Rajesh Balekai <rbalekai@nd.edu>
2014-07-07 13:26:26 -0400

    ETD metadata mapping

--- app/controllers/curation_concern/etds_controller.rb
+++ app/controllers/curation_concern/etds_controller.rb
@@ -1,4 +1,8 @@
 class CurationConcern::EtdsController < CurationConcern::GenericWorksController
   self.curation_concern_type = Etd
 
+  def setup_form
+    super
+    curation_concern.build_degree if curation_concern.degree.blank?
+  end
 end
