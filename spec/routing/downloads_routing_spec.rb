915e930445ca94837726704a019ce8afebdc99a3
Banurekha <banurekha.l@gmail.com>
2014-09-26 11:00:50 -0400

    DLTP-228 download path not include a query parameter
    Adding spec for the route
    
    Fixing failing spec

--- spec/routing/downloads_routing_spec.rb
+++ spec/routing/downloads_routing_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe 'downloads routing' do
   let(:noid) { '1a2b3c' }
+  let(:thumbnail) { 'thumb' }
 
   it "routes GET /downloads/:id" do
     expect(
@@ -12,6 +13,47 @@ describe 'downloads routing' do
         action: "show",
         id: noid
       )
+    )
+  end
+
+  it "routes GET /downloads/:id/:datastream_id" do
+    expect(
+        get: "/downloads/#{noid}/#{thumbnail}"
+    ).to(
+        route_to(
+            controller: "downloads",
+            action: "show",
+            id: noid,
+            datastream_id: thumbnail
+        )
+    )
+  end
+
+  it 'test named route to call action with  id and datastream' do
+    expect(download_path(noid, thumbnail)).to eq("/downloads/#{noid}/#{thumbnail}")
+    expect(
+       get: download_path(noid, thumbnail)
+    ).to(
+        route_to(
+            controller: "downloads",
+            action: "show",
+            id: noid,
+            datastream_id: thumbnail
+        )
+    )
+  end
+
+  it 'test named route with explicit param to call action with correct id and datastream' do
+    expect(download_path(noid, datastream_id: thumbnail)).to eq("/downloads/#{noid}/#{thumbnail}")
+    expect(
+       get: download_path(noid, thumbnail)
+    ).to(
+        route_to(
+            controller: "downloads",
+            action: "show",
+            id: noid,
+            datastream_id: thumbnail
+        )
     )
   end
 end
