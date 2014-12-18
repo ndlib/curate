a2202a5ed4735eeee09f64e424ca4874dd089539
Rajesh Balekai <rbalekai@nd.edu>
2014-12-18 10:32:15 -0500

    Add Creator/Editor to works only during new action
    DLTP-296

--- app/controllers/curation_concern/datasets_controller.rb
+++ app/controllers/curation_concern/datasets_controller.rb
@@ -2,8 +2,10 @@ class CurationConcern::DatasetsController < CurationConcern::GenericWorksControl
   self.curation_concern_type = Dataset
 
   def setup_form
-    curation_concern.creator << current_user.name if curation_concern.creator.empty? && !current_user.can_make_deposits_for.any?
-    curation_concern.editors << current_user.person unless curation_concern.editors.present?
+    if action_name == 'new'
+      curation_concern.creator << current_user.name if curation_concern.creator.empty? && !current_user.can_make_deposits_for.any?
+      curation_concern.editors << current_user.person unless curation_concern.editors.present?
+    end
     curation_concern.editors.build
     curation_concern.editor_groups.build
     curation_concern.viewers.build
